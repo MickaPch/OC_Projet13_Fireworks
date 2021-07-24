5fb1629afcc8529ebde1d7bcea102a5642ba8e22
MickaPch <mickael.perchet@gmail.com>
2021-07-24 19:57:41 +0200

    Test user can login - FAILED

--- user/tests/test_user_view.py
+++ user/tests/test_user_view.py
@@ -5,9 +5,35 @@ from django.test import TestCase
 class UserViewTest(TestCase):
     """Testing user view"""
 
-    def test_user_view(self):
+    def test_user_template_used(self):
         """Test user view"""
 
         self.client.get('/user/')
+
         self.assertTemplateUsed('user.html')
+
+    def test_user_html_status(self):
+        """Test user view"""
+
+        response = self.client.get('/user/')
+
+        self.assertEqual(response.status_code, 200)
     
+    def test_user_page_shows_LogIn(self):
+
+        response = self.client.get('/user/')
+
+        self.assertIn(
+            b'Log In',
+            response.content
+        )
+
+    def test_user_form_is_present(self):
+
+        response = self.client.get('/user/')
+
+        self.assertIn(
+            b'<input',
+            response.content
+        )
+
