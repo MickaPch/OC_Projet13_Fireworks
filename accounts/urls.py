66b48a4188faf69019304902f22ecc94a7eaca8d
MickaPch <mickael.perchet@gmail.com>
2021-08-22 16:53:54 +0200

    User profile + edit forms

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('home/', views.AccountsHomeView.as_view(), name="accounts_home_page"),
     path('profile/', views.AccountsProfileView.as_view(),
          name="accounts_profile_page"),
+    path('edit_username', views.edit_username, name="edit_username"),
+    path('edit_name', views.edit_name, name="edit_name"),
+    path('edit_email', views.edit_email, name="edit_email"),
     path('', include("django.contrib.auth.urls")),
 ]
